3b5d15de0b92c118777d1e24a25659a3ad5966b6
xuuncle <975521377@qq.com>
2017-06-25 11:09:44 +0800

    5. customize admin site

--- polls/models.py
+++ polls/models.py
@@ -12,6 +12,10 @@ class Question(models.Model):
         return self.question_text
     def was_published_recently(self):
         return timezone.now()>=self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
+        
         
         
 class Choice(models.Model):
